4655ea1320d72c39d06c571069117af309b1e91e
Mystery676767 <117195065+Mystery676767@users.noreply.github.com>
2023-01-25 13:39:36 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-
+    function logIn(){
+        var uname = document.getElementById("uName").value;
+        var password = document.getElementById("password").value;
+        sessionStorage.setItem("uName",uname);
+        loadUser();
+    }
+    function loadUser(){
+        var uname1 
+        if (sessionStorage.length==0){
+            uname1 = "log in";
+        } else uname1 = sessionStorage.getItem("uName");
+        document.getElementById("Anmelden").innerHTML = uname1;
+        reload();
+    }
+   
