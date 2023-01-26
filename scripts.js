9f98e705f4e54e9388da6f4215f4789c46928721
Mystery676767 <117195065+Mystery676767@users.noreply.github.com>
2023-01-26 20:23:48 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
-    function logIn(){
-        var uname = document.getElementById("uName").value;
-        var password = document.getElementById("password").value;
-        sessionStorage.setItem("uName",uname);
-        loadUser();
+   function validateName(uname){
+    var serching = uname.split("@");
+    if (serching.length!=2){
+        return false;
+    } else {
+        var serching2 = serching[1].split(".");
+        if (serching2[0].length==0 | serching2[1].length==0 | serching2.length!=2 )  {
+            return false;     
+        } else return true;
     }
-    function loadUser(){
-        var uname1 
-        if (sessionStorage.length==0){
-            uname1 = "log in";
-        } else uname1 = sessionStorage.getItem("uName");
-        document.getElementById("Anmelden").innerHTML = uname1;
-        reload();
-    }
-   
+}
+
+function validatePassword(password){
+    if (password.length<6){
+        return false;
+    } else return true;
+}
+
+function loadUser(){
+    var uname; 
+    alert("test");
+    if (sessionStorage.length==0){
+        uname = "Anmelden";
+    } else uname = sessionStorage.getItem("email");
+    document.getElementById("Anmelden").innerHTML = uname;
+    reload();
+}
+
+function test(){
+    var user = document.getElementById("email");
+    sessionStorage.setItem("email","test");
+    location.href = 'index.html';
+
+}
